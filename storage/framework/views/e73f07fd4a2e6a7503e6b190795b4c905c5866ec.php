e0a7ad041d35bf3b748045d4a83dee57545fde5b
pasahgroup <info@pasah.net>
2025-11-15 13:14:19 +0300

    15 nov 2025

--- storage/framework/views/e73f07fd4a2e6a7503e6b190795b4c905c5866ec.php
+++ storage/framework/views/e73f07fd4a2e6a7503e6b190795b4c905c5866ec.php
@@ -16,7 +16,7 @@
                                 <input type="text" name="username" value="<?php echo e(old('username')); ?>" placeholder="<?php echo app('translator')->get('Username or Email'); ?>" class="form-control form--control" required>
                             </div>
                             <div class="col-md-12">
-                                <label for="password" class="form--label" style="color:red;"><?php echo e(__('Password')); ?></label>
+                                <label for="password" class="form--label" style="color:yellow;"><?php echo e(__('Password')); ?></label>
                                 <input id="password" type="password" class="form-control form--control" placeholder="<?php echo app('translator')->get('Password'); ?>" name="password" required>
                             </div>
 
